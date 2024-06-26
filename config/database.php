1eee276dd21aa452dc7daf6d9ab703d6d352cd08
funway <wang.funway@gmail.com>
2024-06-26 13:37:23 -0700

    修改数据库配置，使用 ssl

--- config/database.php
+++ config/database.php
@@ -54,9 +54,10 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            'options' => extension_loaded('pdo_mysql') ? [
+                PDO::MYSQL_ATTR_SSL_CA  =>'Enable SSL but I dont care about the cert.',
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+            ] : [],
         ],
 
         'mariadb' => [
