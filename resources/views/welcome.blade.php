476a7f8a659a0703bf011fbbe40ddd3f8c6b6e8c
funway <wang.funway@gmail.com>
2024-07-01 10:39:18 -0700

    change welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased dark:bg-black dark:text-white/50">
-        <div class="flex flex-col h-screen bg-gradient-to-r from-gray-800 via-gray-300 to-gray-100">
+        <div class="flex flex-col h-screen">
             <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
                 <div class="px-5 flex lg:justify-center lg:col-start-2">
                     <x-application-logo class="block h-9 w-auto fill-current text-orange-600" />
@@ -49,14 +49,14 @@
                 @endif
             </header>
 
-            <!-- <img id="background" class="absolute -left-20 top-0 max-w-[877px]" src="https://laravel.com/assets/img/welcome/background.svg" /> -->
+            <img id="background" class="absolute -left-20 top-0 max-w-[877px]" src="https://laravel.com/assets/img/welcome/background.svg" />
             <div class="relative h-full flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white pb-32">
-                <main class="">
+                <main class="text-gray-600">
                     <div class="flex flex-col items-center justify-center my-6">
                         <h1 class="text-6xl font-bold mb-4">{{ env('APP_NAME') }}</h1>
                         <!-- <p class="text-sm text-gray-600">Big Brother Is Watching You</p> -->
                     </div>
-                    <blockquote class="border-l-4 border-gray-500 italic text-gray-700 bg-gray-200 p-4">
+                    <blockquote class="border-l-4 border-gray-500 italic bg-gray-200 p-4">
                         "Big Brother Is Watching You."
                         <cite class="block mt-2 text-right text-sm">- George Orwell</cite>
                     </blockquote>
