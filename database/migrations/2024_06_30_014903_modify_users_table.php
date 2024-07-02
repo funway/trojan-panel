b3f5a4a67833919e988d40f8a94b6c193644281c
funway <wang.funway@gmail.com>
2024-07-01 22:34:25 -0700

    [修复] 修复migrate 文件的一个bug，给新增的字段一个默认值。

--- database/migrations/2024_06_30_014903_modify_users_table.php
+++ database/migrations/2024_06_30_014903_modify_users_table.php
@@ -14,11 +14,11 @@ return new class extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->string('name')->unique()->change();
 
-            $table->datetime('expire_at', precision: 0)->comment('订阅过期时间');
+            $table->datetime('expire_at', precision: 0)->default('2024-07-01')->comment('订阅过期时间');
             
-            $table->string('trojan_token', 6)->comment('trojan_password = SHA224(name:trojan_token)');
+            $table->string('trojan_token', 6)->default('abc123')->comment('trojan password = SHA224(name:trojan_token)');
 
-            $table->string('subscription_token');
+            $table->string('subscription_token')->default('123abc');
         });
     }
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,12 +9,21 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <style>
+            /* You can also add the background image directly in the CSS */
+            .bg-custom {
+                background-image: url('https://laravel.com/assets/img/welcome/background.svg');
+                background-repeat: no-repeat;
+                background-position: left top;
+                background-size: auto;
+            }
+        </style>
 
         <!-- Styles -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased dark:bg-black dark:text-white/50">
-        <div class="flex flex-col h-screen">
+        <div class="flex flex-col h-screen bg-custom">
             <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
                 <div class="px-5 flex lg:justify-center lg:col-start-2">
                     <x-application-logo class="block h-9 w-auto fill-current text-orange-600" />
@@ -49,7 +58,7 @@
                 @endif
             </header>
 
-            <img id="background" class="absolute -left-20 top-0 max-w-[877px]" src="https://laravel.com/assets/img/welcome/background.svg" />
+            <!-- <img id="background" class="absolute -left-20 top-0 max-w-[877px]" src="https://laravel.com/assets/img/welcome/background.svg" /> -->
             <div class="relative h-full flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white pb-32">
                 <main class="text-gray-600">
                     <div class="flex flex-col items-center justify-center my-6">
